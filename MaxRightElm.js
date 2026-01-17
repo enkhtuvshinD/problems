134da9ccbfc96815438890ae53e9c5bb49923173
enkhtuvshinD <87625210+enkhtuvshinD@users.noreply.github.com>
2026-01-17 23:48:04 +0800

    Create MaxRightElm.js

---
+++ MaxRightElm.js
@@ -0,0 +1,13 @@
+function replaceElements(arr: number[]): number[] {let n = arr.length;
+        let ans = new Array(n);
+        for (let i = 0; i < n; i++) {
+            let rightMax = -1;
+            for (let j = i + 1; j < n; j++) {
+                rightMax = Math.max(rightMax, arr[j]);
+            }
+            ans[i] = rightMax;
+        }
+                                                   
+        return ans;
+    
+};
